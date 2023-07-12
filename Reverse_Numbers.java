bb25597220a8244a34bf4ea98e04c2275e6e918d
priyankaselvamurugan2003 <139209833+priyankaselvamurugan2003@users.noreply.github.com>
2023-07-12 14:19:34 +0530

    Add files via upload

---
+++ Reverse_Numbers.java
@@ -0,0 +1,13 @@
+public class Reverse_Numbers
+{
+	public static void main(String[] args)
+	{
+		int s=100;
+		int e=1;
+		while(s>=e)
+		{
+			System.out.println(s);
+			s--;
+		}
+	}
+}
---
+++ skipping.java
@@ -0,0 +1,10 @@
+public class skipping{
+    public static void main(String args[])
+    { 
+        int n=1,i;
+        for(i=109;i>n;i=i-2)
+        {
+            System.out.println(i);
+        }
+}
+}
