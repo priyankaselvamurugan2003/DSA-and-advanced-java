7b1336a8dd8461c9f3784afade90971fa8fcaa08
priyankaselvamurugan2003 <139209833+priyankaselvamurugan2003@users.noreply.github.com>
2023-07-21 10:04:04 +0530

    Add files via upload

---
+++ digitcount.java
@@ -0,0 +1,10 @@
+import java.io*;
+import java.util.*;
+public class digitcount
+{
+   public int allDigitsCount(int input1)
+{
+   String str=Integer.toString(input1);
+   return str.length();
+  }
+}
---
+++ palindromeis.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import  java.util.*;
+public class palindromeis
+{
+public int isPalindrome(String input1){
+String str=input1.toLowerCase();
+        String str1="";
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            str1+=str.charAt(i);
+        }
+        if(str.equals(str1))
+        return 2;
+        else
+        return 1;
+    }
